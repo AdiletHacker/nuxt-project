f7559c0d30781473cd6b3a7feaa80588b070364d
EddieBlock <adiletbeyshekeev@mail.ru>
2020-10-27 21:17:42 -0700

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,7 @@ export default {
   apollo: {
     clientConfigs: {
       default: {
-        httpEndpoint: 'http://localhost:1337/graphql'
+        httpEndpoint: "https://cryptic-woodland-53964.herokuapp.com/graphql" || 'http://localhost:1337/graphql'
       }
     }
   },
